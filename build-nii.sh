57b7ab0b4efd74e7e33440284ea09365ad6d0393
Richard Potter <potter@axsh.net>
2016-01-27 19:03:55 +0900

    Put option on wget to output fewer lines of progress info

--- build-nii.sh
+++ build-nii.sh
@@ -64,7 +64,8 @@ DATADIR="$DATADIR" "$ORGCODEDIR/ind-steps/build-1box/build-1box.sh"
 	    $skip_step_if_already_done ; set -e
 
 	    "$DATADIR/vmdir/ssh-to-kvm.sh" <<'EOF'
-wget https://3230d63b5fc54e62148e-c95ac804525aac4b6dba79b00b39d1d3.ssl.cf1.rackcdn.com/Anaconda3-2.4.1-Linux-x86_64.sh
+wget  --progress=dot:mega \
+   https://3230d63b5fc54e62148e-c95ac804525aac4b6dba79b00b39d1d3.ssl.cf1.rackcdn.com/Anaconda3-2.4.1-Linux-x86_64.sh
 
 chmod +x Anaconda3-2.4.1-Linux-x86_64.sh
 
