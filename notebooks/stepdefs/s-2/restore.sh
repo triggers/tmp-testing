e2325a0d6afdd8a0f5e33003e8a2a94d2a54efc3
Richard Potter <potter@axsh.net>
2016-02-03 17:19:12 +0900

    Added demoable hint and restore features to step (2)

---
+++ notebooks/stepdefs/s-2/restore.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+thescript="$(cat <<EEE
+cat <<EOS >vifs.json
+{
+ "eth0":{"network":"nw-demo1","security_groups":"sg-cicddemo"}
+}
+EOS
+
+reset-wakame.sh
+
+sleep 10
+
+mussel instance create --cpu-cores 2 --hypervisor openvz \
+    --image-id wmi-centos1d64 --memory-size 2048 \
+    --ssh-key-id ssh-cicddemo --vifs vifs.json --display-name centos
+EEE
+)"
+
+echo "Now executing the following script to do this step:"
+echo "$thescript"
+echo
+echo "Output follows:"
+
+bash <<<"$thescript"
