d6b4e36c1e8110fa92c0a463156579cff6a47ede
toros11 <tobias.rosen@axsh.net>
2016-02-16 20:09:23 +0900

    Added steps for 104 (git) 105 (hipchat)

---
+++ notebooks/stepdefs/s-jenkins-check-plugins-hipchat/check.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+. /home/centos/notebooks/stepdefs/jenkins-utility/functions.sh
+
+SSH="ssh root@10.0.2.100 -i /home/centos/mykeypair"
+
+${SSH} <<EOF 2> /dev/null
+
+$(declare -f check_plugins_exists)
+
+if ! check_plugins_exists "hipchat" ; then
+    echo "Missing plugin."
+else
+    echo "Configuration is correct"
+fi
+
+EOF
