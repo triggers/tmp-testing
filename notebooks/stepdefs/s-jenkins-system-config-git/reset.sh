39d247ce19d275793fd1f5fc158dd8b3bf9fa2b8
toros11 <tobias.rosen@axsh.net>
2016-02-16 21:19:23 +0900

    Added reset steps for system configurations

---
+++ notebooks/stepdefs/s-jenkins-system-config-git/reset.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+. stepdefs/jenkins-utility/functions.sh
+
+XML_FILE="hudson.plugins.git.GitSCM.xml"
+SSH="ssh root@10.0.2.100 -i /home/centos/mykeypair"
+
+${SSH} [[ ! -f /home/${XML_FILE} ]] && scp -i /home/centos/mykeypair stepdefs/jenkins-config/${XML_FILE} root@10.0.2.100:/home &> /dev/null
+
+${SSH} <<EOF 2> /dev/null
+
+$(declare -f check_client_exists)
+$(declare -f reset_job)
+cp /home/${XML_FILE} /var/lib/jenkins
+
+service jenkins restart
+
+EOF
