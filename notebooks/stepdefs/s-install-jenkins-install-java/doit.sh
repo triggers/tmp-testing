5f41fb8b59ac8adb9912beaf834980f3e97a95f3
Richard Potter <potter@axsh.net>
2016-02-18 12:19:21 +0900

    Added scripts for s-install-jenkins-install-java

---
+++ notebooks/stepdefs/s-install-jenkins-install-java/doit.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+thescript="$(cat <<'EEE'
+# This installs the Oracle version of Java.  The RPM file was
+# already downloaded to /home/centos/notebooks/downloads
+
+: ${IP:=10.0.2.100}
+cd /home/centos/notebooks/downloads
+tar c jdk-8u73-linux-x64.rpm | ssh -qi /home/centos/mykeypair root@$IP tar xv
+
+ssh -qi /home/centos/mykeypair root@$IP <<EOS
+
+rpm -ivh jdk-8u73-linux-x64.rpm
+
+cat <<CFG >/etc/profile.d/java.sh
+JAVA_HOME=/usr/java/jdk1.8.0_25/
+PATH=$JAVA_HOME/bin:$PATH
+export PATH JAVA_HOME
+export CLASSPATH=.
+CFG
+
+EOS
+EEE
+)"
+
+echo "Now executing the following script to do this step:"
+echo "$thescript"
+echo
+echo "Output follows:"
+
+bash <<<"$thescript"
+
+echo "Completed."
