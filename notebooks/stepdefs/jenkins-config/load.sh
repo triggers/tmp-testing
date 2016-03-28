0870b5b877cf29527934d97e2f6b93198116af8e
toros11 <tobias.rosen@axsh.net>
2016-03-28 21:52:10 +0900

    Adjustments to backup functionallity

--- notebooks/stepdefs/jenkins-config/load.sh
+++ notebooks/stepdefs/jenkins-config/load.sh
@@ -3,42 +3,43 @@
 # set -euox
 
 . $(dirname $0)/stepdata.conf
+. ../jenkins-utility/xml-utility.sh
 
 reboot=false
 
 function load_config() {
     local file="/var/lib/jenkins/jobs/${job}/config.xml"
-    local element_name="${1}"
-    local element_value="${2}"
-    ssh -i /home/centos/mykeypair root@10.0.2.100 <<EOF
+    local element_name="${1}" element_value="${2}"
+    ssh -i /home/centos/mykeypair root@10.0.2.100 <<EOF  # 2> /dev/null
+        $(declare -f xml_load_backup)
 
-    # In case there is a element without closing tag we need to remove with a different
-    # match than when removing a code block
-
-        has_end=\$(grep "</${element_name}>" ${file})
-        echo \$has_end
-        [[ -z \$has_end ]] && sed -i '/'${element_name}'/c\' ${file}
-
-    # Remove existing element if exists to avoid duplicates
-    # in case of loading the same block multiple times.
-
-        sed -i '/<'${element_name}'/,/<\/'${element_name}'>/d' ${file}
-
-    # TODO: revise wether this is necessary or not
-
-        cat <<EOS > xml-data.dat
-$(cat xml-data.dat)
-EOS
-        sed -i '/<project>/r xml-data.dat' ${file}
-        yes | rm xml-data.dat
-        $reboot && service jenkins restart
+        xml_load_backup "${file}" "${element_name}" "${element_value}"
 EOF
 }
 
-[[ ${#config_params} -eq 0 ]] || {
-    for param in "${config_params[@]}" ; do
+[[ ${#load_params} -eq 0 ]] || {
+    for param in "${load_params[@]}" ; do
         load_config "${param%%=*}" "${param#*=}"
     done
 }
 
 
+
+#     local old_ifs=$IFS IFS="\n"
+#     while read -r line ; do
+# 	[[ "${line}" == *"/"* ]] && echo ${line}
+#         element_value="$element_value${line}\n"
+#     done <<< "$2"
+#     IFS=$old_ifs
+
+#     element_value=${element_value%\\n*}
+
+
+#         has_end=\$(grep "</${element_name}>" ${file})
+#          [[ -z \$has_end ]] && { sed -i '/'${element_name}'/c\'"${element_value}"'' ${file} ; } || {
+# 	 echo fdsf
+# #            sed -n '/'${element_name}'/{:a;N;/'${element_name}'/!ba;N;s/.*\n/'"${element_value}"'\n/};p'
+# #            sed '/<'${element_name}'/,/<\/'${element_name}'>/p' ${file}
+#          }
+
+# EOF
--- notebooks/stepdefs/jenkins-utility/xml-utility.sh
+++ notebooks/stepdefs/jenkins-utility/xml-utility.sh
@@ -1,14 +1,5 @@
 #!/bin/bash
 
-function replace_git_repo() {
-    local file="${1}" user_git_repo="https://github.com/${GITHUB_USER}/${GITHUB_REPO}.git"
-    local default_repo=$(cat /home/centos/notebooks/stepdefs/jenkins-config/${file} | grep -oP '(?<=<url>).*?(?=</url>)')
-
-    ${ssh} <<EOF 2> /dev/null
-        sed -i "s@${default_repo}@${user_git_repo}@g" /home/${file}
-EOF
-}
-
 function xml_to_vm() {
     for file in "${xml_file[@]}"; do
         scp -i /home/centos/mykeypair /home/centos/notebooks/stepdefs/jenkins-config/${file} root@10.0.2.100:/home &> /dev/null
@@ -16,6 +7,91 @@ function xml_to_vm() {
             replace_git_repo ${file}
         fi
     done
+}
+
+function xml_save_backup () {
+    local file="${1}"
+    local element_name="${2}"
+    local param="${3}"
+
+     if [[ "${param}" == *"node"* ]] ; then
+         config_param=$(sed -n '/<'${element_name}'/,/<\/'${element_name}'>/p' "${file}")
+     else
+         config_param=$(grep -oP '(?<=<'${element_name}'>).*?(?=</'${element_name}'>)' "${file}")
+     fi
+
+    echo "${param}=${config_param}"
+}
+
+function xml_load_backup () {
+    targetfile="$1"
+    elementname="$2"
+    replacementtext="$3"
+
+    reportfailed()
+    {
+        echo "Script failed...exiting. ($*)" 1>&2
+        exit 255
+    }
+    
+    [ -f "$targetfile" ] || reportfailed "XML file $1 not found"
+    
+    
+    # element on one line
+    pattern1a="*<$elementname/>*"
+    pattern1b="*<$elementname */>*"
+    pattern1c="*<$elementname>*</$elementname>*"
+    
+    # element on multiple line
+    pattern2a="*<$elementname>*"
+    pattern2b="*<$elementname *"
+    endpattern1="*</$elementname>*"
+    
+    # Current Assumptions:
+    # - The target element only appears once.
+    # - Nothing unrelated to the target element appears on the same line as its markers.
+    #   (Because those lines get deleted entirely)
+    # - The elementname can not appear as the last line in the file, or else the
+    #   code will add a new line, whether one existed in the original file or not.
+    
+    mv "$targetfile"  "$targetfile.org"
+    exec <"$targetfile.org"
+    
+    replacedit=false
+    while IFS= read -r ln; do
+        case "$ln" in
+            $pattern1a | $pattern1b | $pattern1b)
+                $replacedit && reportfailed "target element $elementname appeared twice"
+                # just replace this one line
+                printf "%s\n" "$replacementtext"
+            replacedit=true
+            ;;
+            $pattern2a | $pattern2b)
+                $replacedit && reportfailed "target element $elementname appeared twice"
+                # scan until end pattern
+                foundit=false
+                while IFS= read -r ln; do
+                    [[ "$ln" == $endpattern1 ]] && foundit=true && break
+                done
+                $foundit || reportfailed "Matching </$elementname>" not found
+                # insert the rest here
+            printf "%s\n" "$replacementtext"
+            replacedit=true
+            ;;
+            *)
+                printf "%s\n" "$ln"
+                ;;
+        esac
+        if $replacedit; then
+            # we are using cat here to output the rest of the file, because
+            # jenkins is sensitive to whether a newline appears at the end
+            # of the file.  It is hard to make bash handle that case, so
+            # let cat handle it and then exit.
+            # (Therefore the "appeared twice" message above will never happen, BTW)
+            cat
+            break
+        fi
+    done >"$targetfile"
 }
 
 # Compares a string value ($1) to all values in an array ($2)
@@ -30,7 +106,6 @@ function contains_value() {
     done
     return 1
 }
-
 
 # Read and returns values from a xml at specified element ($1) where value spans
 # accross multiple lines or element has attribues attatched.
@@ -78,8 +153,8 @@ function confirm() {
     [[ ! -z ${element_name} ]] || { echo "No element specified" ; return 1 ; }
 
     function single_line_value() {
-        local parsed_value=$(cat ${try_xml} | grep -oP '(?<=<'${element_name}'>).*?(?=</'${element_name}'>)')
-        local target_value=$(cat ${target_xml} | grep -oP '(?<=<'${element_name}'>).*?(?=</'${element_name}'>)')
+        local parsed_value=$(grep -oP '(?<=<'${element_name}'>).*?(?=</'${element_name}'>)' <<< ${try_xml})
+        local target_value=$(grep -oP '(?<=<'${element_name}'>).*?(?=</'${element_name}'>)' <<< ${target_xml})
         [[ "${parsed_value}" == "${target_value}" ]]
     }
 
@@ -117,7 +192,6 @@ function confirm() {
         # TODO: Switch to compare the contents of the arrays instead
         [[ "${parsed_values[@]}" == "${target_values[@]}" ]]
     }
-
 
     case "${method}" in
         "single") single_line_value ;;
