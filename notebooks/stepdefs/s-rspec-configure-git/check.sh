b55b4e8a9ad94eaa4006151131d0017bd5663147
toros11 <tobias.rosen@axsh.net>
2016-03-29 22:11:27 +0900

    Updates to check script for rspec-configure-git set

--- notebooks/stepdefs/s-rspec-configure-git/check.sh
+++ notebooks/stepdefs/s-rspec-configure-git/check.sh
@@ -1,3 +1,13 @@
 #!/bin/bash
 
-bash $(dirname $0)/save.sh 
+# TODO: make this script be called only when
+# the previously executed cell was a "provided-script cell"
+bash $(dirname $0)/save.sh
+
+. $(dirname $0)/stepdata.conf
+. /home/centos/notebooks/stepdefs/jenkins-utility/functions.sh
+
+ssh -i /home/centos/mykeypair root@10.0.2.100 <<EOF  2> /dev/null
+    $(declare -f check_not_empty)
+    check_not_empty ${job} url && echo "Check [ ok ]" || echo "Check [ fail ]"
+EOF
