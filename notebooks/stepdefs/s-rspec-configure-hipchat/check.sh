02fff8703697752d988afd2dcd36faeda2fd3255
toros11 <tobias.rosen@axsh.net>
2016-03-31 23:25:37 +0900

    Fixed but that caused second checkt to pass when no notes are added

--- notebooks/stepdefs/s-rspec-configure-hipchat/check.sh
+++ notebooks/stepdefs/s-rspec-configure-hipchat/check.sh
@@ -8,8 +8,13 @@
 ssh -i /home/centos/mykeypair root@10.0.2.100 <<EOF 2> /dev/null
     $(declare -f check_param_value)
 
-    check_param_value notificationType "SUCCESS STARTED FAILURE" ${job} && echo "Added notifications: Passed" || echo "Check [ fail ]"
+    check_param_value notificationType "SUCCESS STARTED FAILURE" ${job} && {
+         echo "Added notifications: Passed"
+         all_types_added=true
+     } || echo "Check [ fail ]"
 
-    # Check for what should not be here and return if found.
-    check_param_value notifyEnabled "false" ${job} && echo "Check [ fail ]" || echo "Enabled notifications: Passed"
+    # We need to make sure the jobs are created to pass this test as well.
+    \$all_types_added && ! check_param_value notifyEnabled "false" ${job} && {
+        echo "Enabled notifications: Passed"
+     } || echo "Check [ fail ]"
 EOF
