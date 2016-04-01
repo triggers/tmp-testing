3035dd51c0f800969815af6bfd8691e2ed8418e3
toros11 <tobias.rosen@axsh.net>
2016-04-01 17:07:25 +0900

    Cleaned up helper functions

--- notebooks/stepdefs/jenkins-utility/functions.sh
+++ notebooks/stepdefs/jenkins-utility/functions.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-# . /home/centos/notebooks/stepdefs/jenkins-utility/xml-utility.sh
 
 original='\033[0m'
 red='\033[00;31m'
@@ -7,23 +6,10 @@ green='\033[00;32m'
 check_mark="[${green}\xE2\x9C\x93${original}]"
 cross_mark="[${red}\xE2\x9C\x97${original}]"
 
-function install_client () {
-    [[ ! -f jenkins-cli.jar ]] &&
-        curl -O http://localhost:8080/jnlpJars/jenkins-cli.jar
-}
-
 function check_plugins_exists () {
     for plugin in $@;  do
         if [[ ! -f /var/lib/jenkins/plugins/${plugin}.jpi ]]; then
             return 1
-        fi
-    done
-}
-
-function install_plugins () {
-    for plugin in $@;  do
-        if [[ ! -f /var/lib/jenkins/plugins/${plugin}.jpi ]]; then
-            java -jar jenkins-cli.jar -s http://localhost:8080 install-plugin ${plugin}
         fi
     done
 }
@@ -46,32 +32,23 @@ function check_param_value() {
     return 0
 }
 
+# Recieves a job name ($1), number of times ($2) the pattern needs to be found
+# and a list of keywords that a line should consist of
 function check_find_line_with() {
     local job="${1}" ; shift
     local passed_check=
-    local occurances=1
-
+    local occurances="${1}" ; shift
     let found=0
+
     while read -r line ; do
         passed_check=true
         for keyword in $@ ; do
-            [[ ${line} != *"${keyword}"* ]] && passed_check=false
+            [[ ${line} != *"${keyword}"* ]] && { passed_check=false ; break ; }
         done
         $passed_check && {
             found=$(( $found+1 ))
             [[ $found -eq $occurances ]] && return 0
         }
     done < /var/lib/jenkins/jobs/${job}/config.xml
-
     return 1
-}
-
-function reset_job () {
-    local job=${1} cfg=${2}
-
-    if [[ -d /var/lib/jenkins/jobs/${1} ]] ; then
-        java -jar jenkins-cli.jar -s http://localhost:8080 delete-job ${job}
-    fi
-    echo "Creating default job for ${job}..."
-    java -jar jenkins-cli.jar -s http://localhost:8080 create-job ${job} < /home/${cfg}
 }
