879c366bdfb86f083987cb770454afcbd229bbd1
toros11 <tobias.rosen@axsh.net>
2016-04-01 17:09:14 +0900

    Added occur argument

--- notebooks/stepdefs/s-rspec-script-prepare-database/check.sh
+++ notebooks/stepdefs/s-rspec-script-prepare-database/check.sh
@@ -7,5 +7,5 @@
 
 ssh -i /home/centos/mykeypair root@10.0.2.100 <<EOF  2> /dev/null
     $(declare -f check_find_line_with)
-    check_find_line_with ${job} mysqladmin create tiny_web_example && echo "Check [ ok ]" || echo "Check [ fail ]"
+    check_find_line_with ${job} 1 mysqladmin create tiny_web_example && echo "Check [ ok ]" || echo "Check [ fail ]"
 EOF
